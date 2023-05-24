8351a390984bbc29c9d59df289e0efe359bfe9f7
Douglas Rodrigues <douglas.r129@outlook.com>
2023-05-24 16:29:59 -0300

    Config StoryBook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,24 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-		node: true,
-		'jest/globals': true,
-	},
-	extends: [
-		'eslint:recommended',
-		'plugin:react/recommended',
-		'plugin:react-hooks/recommended',
-		'plugin:prettier/recommended',
-	],
-	overrides: [],
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	plugins: ['react', 'jest', 'prettier', 'react-hooks'],
-	rules: {
-		indent: ['error', 'tab'],
-		'linebreak-style': ['error', 'unix'],
-		quotes: ['error', 'single'],
-		semi: ['error', 'always'],
-	},
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    'jest/globals': true
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
+  overrides: [],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', 'jest', 'prettier', 'react-hooks'],
+  rules: {
+    indent: ['error', 'tab'],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always']
+  }
 };
---
+++ src/components/Heading/index.jsx
@@ -0,0 +1,12 @@
+import P from 'prop-types';
+import React from 'react';
+import * as Styled from './styles';
+
+export const Heading = ({ children, light = false }) => {
+	return <Styled.Title light={light}>{children}</Styled.Title>;
+};
+
+Heading.propTypes = {
+	children: P.node.isRequired,
+	light: P.bool.isRequired,
+};
