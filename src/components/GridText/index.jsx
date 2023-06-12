89f458c2a460b60122d0789fbbfc658beb40684e
Douglas Rodrigues <douglas.r129@outlook.com>
2023-06-12 17:47:33 -0300

    creat gallery

---
+++ src/components/GridText/index.jsx
@@ -0,0 +1,42 @@
+import P from 'prop-types';
+import * as Styled from './GridText.styles';
+import { SectionBackground } from '../SectionBackground';
+import { Heading } from '../Heading';
+import { TextComponent } from '../TextComponent';
+
+export const GridText = ({ title, description, grid, background = false }) => {
+	return (
+		<SectionBackground background={background}>
+			<Styled.Container>
+				<Heading size="huge" uppercase colorDark={!background} as="h2">
+					{title}
+				</Heading>
+				<TextComponent colorDark={!background}>{description}</TextComponent>
+				<Styled.Grid>
+					{grid.map((el) => (
+						<Styled.GridElement key={el.title}>
+							<Heading size="medium" colorDark={!background} as="h3">
+								{el.title}
+							</Heading>
+							<TextComponent colorDark={!background}>
+								{el.description}
+							</TextComponent>
+						</Styled.GridElement>
+					))}
+				</Styled.Grid>
+			</Styled.Container>
+		</SectionBackground>
+	);
+};
+
+GridText.propTypes = {
+	background: P.bool,
+	title: P.string.isRequired,
+	description: P.string.isRequired,
+	grid: P.arrayOf(
+		P.shape({
+			title: P.string.isRequired,
+			description: P.string.isRequired,
+		}),
+	).isRequired,
+};
--- src/components/TextComponent/index.jsx
+++ src/components/TextComponent/index.jsx
@@ -2,7 +2,12 @@ import P from 'prop-types';
 import * as Styled from './TextComponent.style';
 
 export const TextComponent = ({ children, colorDark = true }) => {
-	return <Styled.Container colorDark={colorDark}>{children}</Styled.Container>;
+	return (
+		<Styled.Container
+			colorDark={colorDark}
+			dangerouslySetInnerHTML={{ __html: children }}
+		/>
+	);
 };
 
 TextComponent.propTypes = {
