6d2cb91837d16fad05c93a18b40effc34f81e3b7
Douglas Rodrigues <douglas.r129@outlook.com>
2023-06-16 16:33:57 -0300

    Alter page init

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,19 +1,42 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Base } from '../Base';
 import Mock from '../Base/Base.mock';
 import { mapData } from '../../api/map-data';
 
 function Home() {
+	const [data, setData] = useState([]);
+	const isMounted = useRef(true);
+
 	useEffect(() => {
 		const load = async () => {
-			const data = await fetch('http://localhost:1337/api/pages?populate=deep');
+			console.log('fetching');
+			const data = await fetch(
+				'http://localhost:1337/api/pages?slug=my-new-page&populate=deep',
+			);
 			const json = await data.json();
-			const pageData = mapData(json.data);
-			console.log(pageData);
+			const { attributes } = json.data[0];
+			const pageData = mapData([attributes]);
+			setData(() => pageData[0]);
 		};
-		load();
+
+		if (isMounted.current === true) {
+			load();
+		}
+
+		return () => {
+			isMounted.current = false;
+		};
 	}, []);
+
+	if (data === undefined) {
+		return <h1>Página não encontrada</h1>;
+	}
+
+	if (data && !data.slug) {
+		return <h1>Carregando...</h1>;
+	}
+
 	return <Base {...Mock} />;
 }
 
